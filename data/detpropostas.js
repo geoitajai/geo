f3f84e3d21918ca06c99276177e262d96fe2786b
jaceguay <jaceguay@gmail.com>
2022-06-30 15:07:11 -0300

    urb

--- data/detpropostas.js
+++ data/detpropostas.js
@@ -1,27 +1,28 @@
 detpropostas = L.featureGroup();
 
-//Proposta engenharia de Trânsito
-$.getJSON("data/detprop.geojson", (data) => {
-    L.geoJson(data, {
-        style: (feature) => {
-            //cor
-            let colinha;
-            cormz = feature.properties.tipoint;
-            if (cormz === 'Nova Ligação') { colinha = "#ff4759"; }
-            else if (cormz === 'Nova Ligação (Variante)') { colinha = "#820002"; }
-            else if (cormz === 'Alargamento') { colinha = "#61ff6e"; }
-            else if (cormz === 'Mudança de Sentido') { colinha = "#ffb031"; }
-            else if (cormz === 'Reurbanização') { colinha = "#3ffff3"; }
-            else { colinha = "#fff"; };
-            //tipo de linha
-            let tipolinha;
-            if (cormz === 'Nova Ligação (Variante)') { tipolinha = "10 10"; }
-            else { tipolinha = null; };
-            //estilo
-            return { color: colinha, weight: 4, dashArray: tipolinha, opacity: 0.65, interactive: false };
-        },
-        pane: 'basemap_overlay'
-    }).addTo(detpropostas);
-});
+L.esri.featureLayer({
+    url: 'https://arcgis.itajai.sc.gov.br/server/rest/services/Hosted/propostasintervencoesvi%C3%A1rias_view/FeatureServer/0/',
+    fields: ['tipoint', 'objectid'],
+    precision: 6,
+    pane: 'marker_overlay',
+    style: (feature) => {
+        //cor
+        let colinha;
+        cormz = feature.properties.tipoint;
+        if (cormz === 'Nova Ligação') { colinha = "#ff4759"; }
+        else if (cormz === 'Nova Ligação (Variante)') { colinha = "#820002"; }
+        else if (cormz === 'Alargamento') { colinha = "#61ff6e"; }
+        else if (cormz === 'Mudança de Sentido') { colinha = "#ffb031"; }
+        else if (cormz === 'Reurbanização') { colinha = "#3ffff3"; }
+        else { colinha = "#fff"; };
+        //tipo de linha
+        let tipolinha;
+        if (cormz === 'Nova Ligação (Variante)') { tipolinha = "10 10"; }
+        else { tipolinha = null; };
+        //estilo
+        return { color: colinha, weight: 4, dashArray: tipolinha, opacity: 0.65, interactive: false };
+    },
+    pane: 'basemap_overlay'
+}).addTo(detpropostas);
 
 mapa.addLayer(detpropostas);
